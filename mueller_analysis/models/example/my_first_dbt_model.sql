a009632e5cdd6ede4d27ef5254e71bb2e023c98f
BolandMueller <kboland@muellerroofing.com>
2025-09-16 15:22:11 -0400

    fix(example): filter out null ids in example model so tests pass

--- mueller_analysis/models/example/my_first_dbt_model.sql
+++ mueller_analysis/models/example/my_first_dbt_model.sql
@@ -20,8 +20,5 @@ with source_data as (
 select *
 from source_data
 
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+-- Remove records with null `id` values so example tests pass
+where id is not null
