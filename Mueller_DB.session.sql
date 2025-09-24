45a850308756376e6229646c8eba7427e68ccf35
BolandMueller <kboland@muellerroofing.com>
2025-09-24 09:08:50 -0400

    new file:   mv_oeel_dev.sql

--- Mueller_DB.session.sql
+++ Mueller_DB.session.sql
@@ -1,5 +1,20 @@
 SELECT pg_get_viewdef('public.mv_oeel', true);
 
 
-create materialized view dev_mueller.mv_oeel as
+create materialized view dev_mueller.test1 as
 select * from public.mv_oeel;
+
+
+SELECT current_database() AS db, current_user AS who, session_user AS session_who;
+
+
+
+SELECT schema_name, schema_owner
+FROM information_schema.schemata
+WHERE schema_name = 'dev_mueller';
+
+SELECT schemaname, matviewname
+FROM pg_matviews
+WHERE matviewname = 'test1';
+
+SELECT has_schema_privilege(current_user, 'dev_mueller', 'CREATE') AS can_create_in_schema;
---
+++ mv_oeel_dev.sql
@@ -0,0 +1,61 @@
+-- dev.mv_oeel source
+
+CREATE MATERIALIZED VIEW dev.mv_oeel
+TABLESPACE pg_default
+AS SELECT COALESCE(oeel.invoicedt, '1990-01-01'::date) AS invoicedt,
+    oeel.enterdt,
+    oeel.orderno::character varying(12) AS orderno,
+    oeel.ordersuf::smallint AS ordersuf,
+    oeel.lineno::smallint AS lineno,
+    oeel.custno,
+    oeel.whse::character varying(4) AS whse,
+    icsd.city::character varying(32) AS whsecity,
+    upper(oeel.slsrepout::text)::character(4) AS slsrepout,
+    upper(oeel.transtype::text)::character(2) AS transtype,
+    upper(oeel.prodcat::character(4)::text) AS prodcat,
+    upper(oeel.shipprod::text)::character varying(24) AS shipprod,
+    oeel.price::numeric(12,4) AS price,
+        CASE
+            WHEN oeel.priceoverfl = 1 THEN true
+            ELSE false
+        END AS priceoverfl,
+    round(oeel.qtyship *
+        CASE
+            WHEN oeel.returnfl = 1 THEN '-1'::integer
+            ELSE 1
+        END::numeric, 4)::numeric(12,4) AS qtyship,
+    round(oeel.stkqtyship *
+        CASE
+            WHEN oeel.returnfl = 1 THEN '-1'::integer
+            ELSE 1
+        END::numeric, 4)::numeric(12,4) AS stkqtyship,
+    oeel.unit,
+    oeel.unitconv,
+    round(oeel.netamt *
+        CASE
+            WHEN oeel.returnfl = 1 THEN '-1'::integer
+            ELSE 1
+        END::numeric, 4)::numeric(12,4) AS netamt,
+    round(oeel.prodcost * oeel.qtyship *
+        CASE
+            WHEN oeel.returnfl = 1 THEN '-1'::integer
+            ELSE 1
+        END::numeric, 4)::numeric(12,4) AS prodcost,
+    oeel.prodline,
+    oeel.vendno,
+    COALESCE(( SELECT "SXFamilyGroup"."Short"
+           FROM "SXFamilyGroup"
+          WHERE SUBSTRING(oeel.prodline FROM 1 FOR 2) = "SXFamilyGroup"."Prefix"::text AND SUBSTRING(oeel.prodline FROM 4 FOR 3)::integer >= "SXFamilyGroup"."Low" AND SUBSTRING(oeel.prodline FROM 4 FOR 3)::integer <= "SXFamilyGroup"."High"
+         LIMIT 1), 'UK'::bpchar)::character(2) AS famgrp,
+    oeel.dctransdate::timestamp with time zone AS dctransdate,
+    CURRENT_TIMESTAMP AS lastupdated
+   FROM oeel
+     LEFT JOIN icsd ON oeel.whse = icsd.whse
+  WHERE oeel.cono = 1 AND (oeel.invoicedt >= (CURRENT_TIMESTAMP - '3 years'::interval) OR oeel.invoicedt IS NULL AND oeel.enterdt >= (CURRENT_TIMESTAMP - '3 years'::interval)) AND oeel.canceldt IS NULL AND icsd.salesfl = 1
+  ORDER BY (oeel.orderno::character varying(12)), (oeel.ordersuf::smallint), (oeel.lineno::smallint)
+WITH DATA;
+
+-- View indexes:
+CREATE INDEX idx_mv_oeel_dctransdate ON dev.mv_oeel USING btree (dctransdate DESC);
+CREATE INDEX idx_mv_oeel_invoicedt ON dev.mv_oeel USING btree (invoicedt DESC);
+CREATE UNIQUE INDEX idx_mv_oeel_uniq ON dev.mv_oeel USING btree (orderno, ordersuf, lineno);
